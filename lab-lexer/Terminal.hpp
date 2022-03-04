1a5d0609f46817ae9999f7abaf9de8397539bf93
KH <1319622082@qq.com>
2022-03-04 10:11:16 +0800

    feat: finish Terminal

--- lab-lexer/Terminal.hpp
+++ lab-lexer/Terminal.hpp
@@ -6,8 +6,8 @@ class Terminal {
 public:
     Terminal(const std::string &tag = "< ", const std::string &start_msg = "welcome to use lexer!")
         : tag_(tag), start_msg_(start_msg) {
-            OutPutStartMsg();
-        }
+        OutPutStartMsg();
+    }
 
     std::string Input() {
         std::string str;
@@ -16,19 +16,21 @@ public:
         return str;
     }
 
-    void OutPut(const std::string &str){
-         std::cout << str << std::endl;
-         OutPutTag();
+    void OutPut(const std::string &str) {
+        std::cout << str << std::endl;
+        OutPutTag();
     }
+
 private:
-    void OutPutTag(){
+    void OutPutTag() {
         std::cout << tag_;
     }
-    void OutPutStartMsg(){
+    void OutPutStartMsg() {
         OutPutTag();
         std::cout << start_msg_ << std::endl;
         OutPutTag();
     }
+
 private:
     std::string tag_;
     std::string start_msg_;
